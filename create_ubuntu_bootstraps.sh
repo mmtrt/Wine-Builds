63ea5a3b69207c216e54a97e1367cff607a676d2
Taqi Raza <mttalpur@gmail.com>
2025-01-07 12:08:48 +0500

    update build depend package versions

--- create_ubuntu_bootstraps.sh
+++ create_ubuntu_bootstraps.sh
@@ -66,17 +66,17 @@ prepare_chroot () {
 }
 
 create_build_scripts () {
-	sdl2_version="2.26.4"
-	faudio_version="23.03"
+	sdl2_version="2.30.10"
+	faudio_version="24.12"
 	vulkan_headers_version="1.4.304"
 	vulkan_loader_version="1.4.304"
-	spirv_headers_version="sdk-1.3.239.0"
- 	libpcap_version="1.10.4"
-  	libxkbcommon_version="1.6.0"
-   	python3_version="3.12.4"
-    	meson_version="1.3.2"
-     	cmake_version="3.30.3"
-      	ccache_version="4.10.2"
+	spirv_headers_version="sdk-1.3.296.0"
+	libpcap_version="1.10.5"
+	libxkbcommon_version="1.7.0"
+	python3_version="3.12.8"
+	meson_version="1.6.0"
+	cmake_version="3.31.2"
+	ccache_version="4.10.2"
 
 	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
 #!/bin/bash
